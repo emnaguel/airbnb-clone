47491a1fb62897113fcbe9b4d2ecfd43ceff1b35
Emna Guellaty <eguellaty@gmail.com>
2018-05-22 11:44:08 +0000

    add validations

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,4 +1,15 @@
 class Boat < ApplicationRecord
   belongs_to :user
   CATEGORIES = ['Sailboat', 'Motorboat', 'Catamaran', 'Jet ski', 'Houseboat']
+  validates :name, presence: :true
+  validates :address, presence: :true
+  validates :description, presence: :true
+
+  validates :price, presence: :true
+  validates :model, presence: :true
+  validates :capacity, presence: :true
+
 end
+
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+has_many :boats
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+validates :email, format: { with: /\A.*@.*\.com\z/ }
+validates :encrypted_password, presence: :true
+
 end
