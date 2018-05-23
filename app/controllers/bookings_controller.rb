5c92a043388ed32484e62b10bc97de6a689fa7c0
Ayoub Karioun <kariounayoub@gmail.com>
2018-05-23 15:59:59 +0200

    typed.js flatpickr.js and home change

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -23,6 +23,8 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
     @booking.user = current_user
     @booking.boat = Boat.find(params[:boat_id])
+    @booking.total_price = @booking.boat.price * (@booking.end_date - @booking.start_date)
+    @booking.status = true
     authorize @booking
     @booking.save!
     if @booking.save
@@ -34,6 +36,7 @@ class BookingsController < ApplicationController
 
   def update
     @booking.update(booking_params)
+    @booking.total_price = @booking.boat.price * (@booking.end_date - @booking.start_date)
     authorize @booking
     if @booking.update(booking_params)
       redirect_to booking_path(@booking)
