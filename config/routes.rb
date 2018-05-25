0d5cc5dc1f7c0e4b40d743709286a4edb831377d
Ayoub Karioun <kariounayoub@gmail.com>
2018-05-25 10:56:01 +0200

    scafolf

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   end
   get 'dashboard_renter', to: 'pages#dashboard_renter', as: 'dashboard_renter'
   get 'dashboard_owner', to: 'pages#dashboard_owner', as: 'dashboard_owner'
-  resources :bookings, only: [:index, :edit, :update, :destroy, :show]
+  resources :bookings, only: [:index, :edit, :update, :destroy, :show] do
+    resources :reviews, only: [:create, :new, :edit, :update, :destroy]
+  end
   devise_for :users
   root to: 'pages#home'
 end
